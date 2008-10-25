5b67a0df3a4c14e1a519ca24cb362ad895fd12b9
Alto <boettger@mt7.de>
2008-10-25 18:00:26 +0200

    test fix for some missing end

--- app/models/snotes/twitter.rb
+++ app/models/snotes/twitter.rb
@@ -6,12 +6,6 @@ class Snotes::Twitter
       results = ::Twitter::Search.new.to(query)
       if last_tweet = Tweet.find(:first, :order => 'created_at DESC')
         results = results.since(last_tweet.twitter_id)
-    end
-    results.collect do |result|
-      if result != 'results'
-        user = User.find_or_create!(result['from_user_id'], :name => result['from_user'])
-        Tweet.find_or_create!(result['id'], :message => result['text'], :user_id => user.id,
-          :language => result['iso_language_code'], :image_url => result['profile_image_url'])
       end
       results.collect do |result|
         if result != 'results'
@@ -19,7 +13,14 @@ class Snotes::Twitter
           Tweet.find_or_create!(result['id'], :message => result['text'], :user_id => user.id,
             :language => result['iso_language_code'], :image_url => result['profile_image_url'])
         end
-      end.compact
+        results.collect do |result|
+          if result != 'results'
+            user = User.find_or_create!(result['from_user_id'], :name => result['from_user'])
+            Tweet.find_or_create!(result['id'], :message => result['text'], :user_id => user.id,
+              :language => result['iso_language_code'], :image_url => result['profile_image_url'])
+          end
+        end.compact
+      end
     end
     
     def autofollow
